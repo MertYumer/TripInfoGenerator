8de36f0f00497e032f8673482b7fdf51338ef394
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2020-01-22 11:38:53 +0200

    Initial commit

---
+++ TripInfoGenerator/StartUp.cs
@@ -0,0 +1,143 @@
+﻿namespace TripInfoGenerator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Net;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
+    public class StartUp
+    {
+        public static List<string> towns = new List<string>() {
+                "Blagoevgrad",
+                "Burgas",
+                "Varna",
+                "Veliko Tarnovo",
+                "Vidin",
+                "Vratsa",
+                "Gabrovo",
+                "Dobrich",
+                "Kardzhali",
+                "Kyustendil",
+                "Lovech",
+                "Montana",
+                "Pazardzhik",
+                "Pernik",
+                "Pleven",
+                "Plovdiv",
+                "Razgrad",
+                "Ruse",
+                "Silistra",
+                "Sliven",
+                "Smolyan",
+                "Sofia",
+                "Stara Zagora",
+                "Targovishte",
+                "Haskovo",
+                "Shumen",
+                "Yambol"};
+        public static List<string> urls = new List<string>();
+        public static List<Trip> trips = new List<Trip>();
+        public static string filePath = "../../../trips-info.json";
+
+        public static void Main()
+        {
+            GenerateUrls();
+            SendRequestAndReadResponse();
+            WriteResultToFile();
+            DisplayJson();
+        }
+
+        private static void GenerateUrls()
+        {
+            //Generate url for every pair between the towns
+            for (int i = 0; i < towns.Count; i++)
+            {
+                for (int j = i + 1; j < towns.Count; j++)
+                {
+                    urls.Add($"https://www.google.com/maps/dir/{towns[i]}/{towns[j]}");
+                }
+            }
+        }
+
+        private static void SendRequestAndReadResponse()
+        {
+            for (int i = 0; i < urls.Count; i++)
+            {
+                //Get towns names from url
+                var originMatch = Regex.Match(urls[i], @"dir\/([A-Z][a-z]+( [A-Z][a-z]+)?)\/");
+                var origin = originMatch.Groups[1].Value;
+
+                var destinationMatch = Regex.Match(urls[i], @"\/([A-Z][a-z]+( [A-Z][a-z]+)?)$");
+                var destination = destinationMatch.Groups[1].Value;
+
+                WebRequest request = WebRequest.Create(urls[i]);
+                WebResponse response = request.GetResponse();
+
+                using (Stream dataStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(dataStream);
+                    string responseFromServer = reader.ReadToEnd();
+
+                    //Get distance and estimated time from response
+                    var distanceMatch = Regex.Match(responseFromServer, @"(\d+(,\d+)?) км");
+                    var distance = double.Parse(distanceMatch.Groups[1].Value);
+
+                    var durationMatch = Regex.Match(responseFromServer, @"([0-5][0-9]) мин|(0[0-9]|1[0-9]|2[0-3]|[0-9]) ч ([0-5][0-9]) мин");
+                    var duration = durationMatch.Value;
+
+                    //Create object from type Trip and add it to collection
+                    var trip = new Trip
+                    {
+                        Origin = origin,
+                        Destination = destination,
+                        Distance = distance,
+                        Duration = duration,
+                    };
+
+                    trips.Add(trip);
+
+                    WriteCurrentTripAndCompletionStatus(trip, i);
+
+                    response.Close();
+                }
+
+                //You have to add pause between requests with appropriate interval, otherwise the program gives WebException - Error(429): Too many requests
+                System.Threading.Thread.Sleep(30000);
+            }
+        }
+
+        private static void WriteCurrentTripAndCompletionStatus(Trip trip, int i)
+        {
+            var jsonTrip = JsonConvert.SerializeObject(trip, Formatting.Indented);
+            Console.WriteLine(jsonTrip);
+
+            double percent = (double)((i + 1) * 100 / urls.Count);
+            double percentCompletedRequests = Math.Ceiling(percent);
+            Console.WriteLine($"Completed requests - {percentCompletedRequests}%");
+            Console.WriteLine($"Remaining requests - {urls.Count - (i + 1)}");
+        }
+
+        private static void WriteResultToFile()
+        {
+            using (FileStream fs = File.Create(filePath))
+            {
+                //Serialize the collection and add the JSON object to the given file
+                var jsonResult = JsonConvert.SerializeObject(trips, Formatting.Indented);
+                var tripsInfo = new UTF8Encoding(true).GetBytes(jsonResult);
+                fs.Write(tripsInfo);
+            }
+        }
+
+        private static void DisplayJson()
+        {
+            var jsonContent = File.ReadAllText(filePath);
+            var tripsInfo = JToken.Parse(jsonContent).ToString(Formatting.Indented);
+            Console.WriteLine(tripsInfo);
+        }
+    }
+}
---
+++ TripInfoGenerator/Trip.cs
@@ -0,0 +1,13 @@
+﻿namespace TripInfoGenerator
+{
+    public class Trip
+    {
+        public string Origin { get; set; }
+
+        public string Destination { get; set; }
+
+        public double Distance { get; set; }
+
+        public string Duration { get; set; }
+    }
+}
